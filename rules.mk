c4634e9ee78342c4ac74d7689326d20e16fe2657
Alexey Degtiarev <alexey@degtiarev.com>
2010-09-13 20:25:33 +0400

    temporary commit

--- rules.mk
+++ rules.mk
@@ -12,22 +12,34 @@ DESTFILES := $(patsubst %,$(DESTPATH)%,$(FILES))
 
 .PHONY: $(DESTFILES) $(FILES)
 
+BANNER := 
+
+ifeq ($(COMMAND),)
+ ifeq ($(if $(INSTALL_TYPE),$(INSTALL_TYPE),link),link)
+   BANNER := "Using soft link for target $(CIRDIR)..."
+   COMMAND =  rm -rf $$_d; ln -sf $$_s $$_d
+ endif
+else
+BANNER := "Using custom $$COMMAND for target $(CURDIR)..."
+endif
+
+default: banner $(DESTFILES)
+
 $(DESTFILES): $(FILES)
 	@_d=$(call normd,$@); \
 	_s=$(call norms,$@); \
 	echo -n " * $$_s => $$_d: "; \
-	if [ ! -L $$_d ]; then \
-		echo "file"; \
-		if [ ! -f $$_d -o -n "$$(diff -ur $$_d $$_s)" ]; then \
-			echo " ==> differ"; \
-			diff -ur $$_d $$_s > $${_s}.local-diff; \
-		fi; \
-		if [ -n "$(DO_INSTALL)" ]; then \
-			echo " ==> replacing"; \
-			rm -rf $$_d; ln -sf $$_s $$_d; \
-		fi; \
+	if [ -f $$_d -o -h $$_d ] && [ -n "$$(diff -ur $$_d $$_s)" ]; then \
+		echo " ==> differ"; \
+		diff -ur $$_d $$_s > $${_s}.local-diff; \
 	else \
-		echo "symlink"; \
-	fi
+		[ -f $$_d -o -h $$_d ] && echo " ==> similar"; break;\
+		[ -f $$_d -o -h $$_d ] || echo " is not a file or symlink";\
+	fi; \
+	if [ -n "$(DO_INSTALL)" ]; then \
+		echo " ==> replacing"; \
+		$(COMMAND); \
+	fi; \
 
-
+banner:
+	@echo "$(BANNER)"
